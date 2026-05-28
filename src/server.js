91c3b919c8d62a06f4cd8312983dd92f84a51739
Guilherme L Reinaldo <guilhermeleal938@gmail.com>
2026-05-28 14:48:23 -0400

    chore: support dynamic port binding for cloud deployment

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
